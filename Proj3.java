4336e85aeb36e53324ba861d4e872cf626c311e1
cswisher42 <31708343+cswisher42@users.noreply.github.com>
2017-09-25 19:11:21 -0500

    Add files via upload

--- Proj3.java
+++ Proj3.java
@@ -13,11 +13,16 @@ public class Proj3
 	public static void main(String [] args) throws IOException
 	{
 		Scanner s = new Scanner(System.in);
-		String [] words = new String [50];
 		String name = getFileInfo();
-		
+		String [] words = new String [fileLength(name)];
+		Boolean [] palindrome = new Boolean[fileLength(name)];
 		readFile(name, words);
-		displayResults(words); 
+		//displayResults(words);
+		
+		for(int i = 0; i < words.length; i++)
+		{
+			isPalindrome(words[i]);
+		}
 	} //end main
 	
 	public static String getFileInfo() throws IOException
@@ -58,16 +63,50 @@ public class Proj3
 		
 		int total = 0;
 		
-		while(inFile.hasNext())
+		while(inFile.hasNextLine())
 		{
 			words[total] = inFile.nextLine();
 			total++;
 		}
 	} //end readFile
 	
+	public static Boolean isPalindrome(String word)
+	{
+		word.toLowerCase();
+		StringBuilder sb = new StringBuilder(word);
+		
+		for(int i = 0; i < sb.length(); i++)
+		{
+			if(sb.charAt(i) == ' ' || sb.charAt(i) == ',' || sb.charAt(i) == '.')
+			{
+				sb.deleteCharAt(i);
+			}
+		}
+		System.out.println(sb);
+		return true;
+	} //end isPalindrome
+	
 	public static void displayResults(String [] words)
 	{
 		for(int i = 0; i < words.length; i++)
 			System.out.println(words[i]);
-	}
+	} //end displayResults
+	
+	public static int fileLength(String fileName) throws IOException
+	{
+		File file = new File(fileName);
+		Scanner inFile = new Scanner(file);
+		
+		int lines = 0;
+		
+		while(inFile.hasNextLine())
+		{
+			lines++;
+			inFile.nextLine();
+		}
+		
+		inFile.close();
+		
+		return lines;
+	} //end fileLength
 } //end class
